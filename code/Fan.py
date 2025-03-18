73438be6e8c731831596baec3a2f70511d2420d5
benedikt.frasch <benedikt.frasch@dhbw-engineering.de>
2025-03-18 11:26:34 +0100

    learn python

--- code/Fan.py
+++ code/Fan.py
@@ -1,15 +1,18 @@
 import machine
 
 class Fan():
-    
-    pin
-    pwm
-    
-    __init__(self, pwm_pin):
-        pin = machine.Pin(pwm_pin)
-        pwm = machine.PWM(pin)
-        pwm.freq(1000)
+
+    def __init__(self, pwm_pin):
+        self.pin = machine.Pin(pwm_pin)
+        self.pwm = machine.PWM(self.pin)
+        self.pwm.freq(1000)
         
-    def set_value(duty_cycle):
+    def set_value(self, duty_cycle):
         """@duty_cylce: 1byte value"""
-        pwm.duty(duty_cycle*4)
+        self.pwm.duty(duty_cycle*4)
+        
+fan = Fan(4)
+fan.set_value(128)
+
+while True:
+    pass
--- code/Illumination.py
+++ code/Illumination.py
@@ -2,11 +2,9 @@ import machine
 
 class Illumination():
     
-    pin
-    
-    __init__(self, illumination_pin):
-        pin = machine.Pin(illumination_pin, machine.Pin.OUT)
+    def __init__(self, illumination_pin):
+        self.pin = machine.Pin(illumination_pin, machine.Pin.OUT)
         
     def set_value(set_enable):
         """@set_enable: 0 or 1"""
-        pin.value(set_enable)
+        self.pin.value(set_enable)
