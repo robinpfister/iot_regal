2da5a6a0e7d2364039452ef191ae2bd500519c27
robinpfister <119607482+robinpfister@users.noreply.github.com>
2025-03-18 21:34:02 +0100

    updated WaterTemperature class

--- code/temperature_set.py
+++ code/temperature_set.py
@@ -1,37 +1,23 @@
 from ds18x20 import DS18X20
-from machine import Pin
-from onewire import OneWire
+import time
 
-# addresses = { "Red" : bytearray(b'(\xffd\x1f[\xcd\xa3.'),
-#             "Orange" : bytearray(b'(\xffd\x1f[\xa0\x1aS'),
-#             "Yellow" : bytearray(b"(\xd37\x1c\x91!\x06'"),
-#             "Blue" : bytearray(b'(F\x19V\xb5\x01<\xd7'),
-#             "White" : bytearray(b'(G&V\xb5\x01<\x0b'),
-#             "Green" : bytearray(b'(\xffUV\xb5\x01<\x8e')}
+addresses = { "Red" : bytearray(b'(\xffd\x1f[\xcd\xa3.'),
+             "Orange" : bytearray(b'(\xffd\x1f[\xa0\x1aS'),
+             "Yellow" : bytearray(b'(t\xe0V\xb5\x01<\x01'),
+             "Black" : bytearray(b'(\x98\x17V\xb5\x01<\xa2'),
+             "Blue" : bytearray(b'(F\x19V\xb5\x01<\xd7'),
+             "White" : bytearray(b'(G&V\xb5\x01<\x0b'),
+             "Green" : bytearray(b'(\xffUV\xb5\x01<\x8e')}
 
-adresses_top = {"Left" : bytearray(b'(\xffd\x1f[\xcd\xa3.'),
-                "Middle" : bytearray(b'(\xffd\x1f[\xa0\x1aS'),
-                "Right" : bytearray(b"(\xd37\x1c\x91!\x06'")}
-
-adresses_box = {"Left" : bytearray(b'(F\x19V\xb5\x01<\xd7'),
-                "Middle" : bytearray(b'(G&V\xb5\x01<\x0b'),
-                "Right" : bytearray(b'(\xffUV\xb5\x01<\x8e')}
-
-class TemperatureSet():
-    def __init__(self, bus_pin, addr):
-        self.left_addr = addr["Left"]
-        self.middle_addr = addr["Middle"]
-        self.right_addr = addr["Right"]
-        self.pin = Pin(bus_pin, mode=Pin.IN)
-        self.sensor = DS18X20(OneWire(self.pin))
+class WaterTemperature():
+    def __init__(self, one_wire, addr):
+        self.addr = addr
+        self.sensor = DS18X20(one_wire)
         
     def get_value(self):
+        time.sleep(0.01)
         self.sensor.convert_temp()
-        left_value = self.sensor.read_temp(self.left_addr)
-        middle_value = self.sensor.read_temp(self.middle_addr)
-        right_value = self.sensor.read_temp(self.right_addr)
-        return {"Left" : left_value,
-                "Middle" : middle_value,
-                "Right" : right_value}
+        value = self.sensor.read_temp(self.addr)
+        return value
         
         
